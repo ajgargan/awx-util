e22cf37cb0c3727bfc2c635e9f490ae1793e16b7
Andrew Gargan <agargan@gmail.com>
2018-12-19 15:20:06 +0200

    Update build_awx.sh

--- build_awx.sh
+++ build_awx.sh
@@ -1,39 +1,3 @@
-#!/bin/bash
-
-#Install epel
-yum install -y epel-release
-
-#Install dependencies via yum
-yum install -y --enablerepo epel git gcc docker python27-pip python27-devel libffi-devel openssl-devel git curl util-linux
-
-#Install python dependencies
-/usr/bin/pip install -U docker ansible awscli
-
-#Remove old crap
-rm -rf awx*
-rm -rf build-*
-
-# Check out awx
-if [ ! -d "awx" ]
-then
-	git clone https://github.com/ansible/awx.git
-fi
-
-# Check out awx-logos
-if [ ! -d "awx-logos" ]
-then
-git clone https://github.com/ansible/awx-logos.git
-fi
-
-
-#Run build for each release version of AWX
-for i in $(cat versions.txt)
-do
-	# running in the background
-	./build_awx.sh $i >> build-$i.log 2>&1
-	#/./build_awx.sh $i
-done
-[root@ip-10-36-53-8 testing]# cat build_awx.sh 
 #!/bin/bash -x
 
 # Clear old repos
