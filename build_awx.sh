c3361e8b68f59ff4999ee79fa1860605078a3ab2
Andrew Gargan <agargan@gmail.com>
2018-12-19 11:14:59 +0200

    fix a few silly bugs && wrapper issues

--- build_awx.sh
+++ build_awx.sh
@@ -1,25 +1,76 @@
-#~/bin/bash -x
-# Create build dir
-mkdir -p build
+#!/bin/bash
 
-# Clear old repos
+#Install epel
+yum install -y epel-release
+
+#Install dependencies via yum
+yum install -y --enablerepo epel git gcc docker python27-pip python27-devel libffi-devel openssl-devel git curl util-linux
+
+#Install python dependencies
+/usr/bin/pip install -U docker ansible awscli
+
+#Remove old crap
 rm -rf awx*
+rm -rf build-*
 
 # Check out awx
-git clone https://github.com/ansible/awx.git
+if [ ! -d "awx" ]
+then
+	git clone https://github.com/ansible/awx.git
+fi
 
 # Check out awx-logos
+if [ ! -d "awx-logos" ]
+then
 git clone https://github.com/ansible/awx-logos.git
+fi
+
+
+#Run build for each release version of AWX
+for i in $(cat versions.txt)
+do
+	# running in the background
+	./build_awx.sh $i >> build-$i.log 2>&1
+	#/./build_awx.sh $i
+done
+[root@ip-10-36-53-8 testing]# cat build_awx.sh 
+#!/bin/bash -x
+
+# Clear old repos
+#rm -rf awx*
+
+# Check out awx
+if [ ! -d "awx" ]
+then
+	git clone https://github.com/ansible/awx.git
+fi
+
+# Check out awx-logos
+if [ ! -d "awx-logos" ]
+then
+	git clone https://github.com/ansible/awx-logos.git
+fi
+
+if [ "X$1" != "X" ]
+then
+	version=$1
+else
+	version=latest
+fi
+
+# Create build dir
+mkdir -p build-$version
 
 # Copy projects into build
-cp -dpru awx build/
-cp -drpu awx-logos build/
+cp -v -dpru awx-logos/ build-$version/
+cp -v -dpru awx/ build-$version/
 
-cd build/awx
+cd build-$version/awx
 pwd
+ls -l --color
 
 # if we get given a release checkout that version
-if [ "X$1" != "X" ]
+if [ "$version" != "latest" ]
 then
    git checkout $1 
 
@@ -39,4 +90,4 @@ ansible-playbook -i inventory install.yml
 cd ../../..
 
 # cleanup
-rm -rf build
+#rm -rf build-$version
